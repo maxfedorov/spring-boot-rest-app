ec4b92b2f789548a8b36a8c847840f162be01820
maxfedorov <maksfedorov@mail.ru>
2022-06-14 01:46:05 +0200

    Add Gatling

--- README.md
+++ README.md
@@ -3,6 +3,9 @@
 - Java, RestAssured, Retrofit, SpringMVC, Mockito
 - Run app: `mvn spring-boot:run`
 - Allure report: `mvn allure:serve`
+- Run tests: `mvn clean test`
+- Run Gatling tests: `mvn gatling:test`
+- Gatling report is generated to: `target/gatling`
 - Jacoco report is generated to: `target/site/jacoco/index.html`
 - Swagger ui: `http://localhost:{port}/swagger-ui/index.html`
 - Default port is 8080
---
+++ src/test/java/com/example/springbootrestapp/gatling/simulations/GetUserSimulation.scala
@@ -0,0 +1,36 @@
+package com.example.springbootrestapp.gatling.simulations
+
+import io.gatling.core.Predef._
+import io.gatling.core.structure.ScenarioBuilder
+import io.gatling.http.Predef._
+import io.gatling.http.protocol.HttpProtocolBuilder
+
+import scala.concurrent.duration.DurationInt
+
+class GetUserSimulation extends Simulation {
+
+  val httpConf: HttpProtocolBuilder = http.baseUrl("http://localhost:8080/api/v1/")
+    .header("Accept", "application/json")
+
+  val scn: ScenarioBuilder = scenario("Get user")
+    .during(10.seconds) {
+      exec(http("Get user")
+        .get("users/1")
+        .check(jsonPath("$.id").is("1"))
+        .check(jsonPath("$.firstName").is("John"))
+        .check(jsonPath("$.lastName").is("Parker"))
+        .check(jsonPath("$.email").is("jp@email.com"))
+        .check(status.is(200)))
+    }
+
+  setUp(
+    scn.inject(
+      atOnceUsers(1),
+      rampUsers(10) during 5.seconds
+    ).protocols(httpConf)
+  ).maxDuration(10.seconds)
+    .assertions(
+      global.successfulRequests.percent.gt(95),
+      global.responseTime.max.lt(5000)
+    )
+}
